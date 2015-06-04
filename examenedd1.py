4ad019a926f6acba9d00aa8f06977e53a1012702
miltonlab <1000ton.lab@gmail.com>
2015-06-04 18:08:26 -0500

    completadas las dos preguntas practicas

--- examenedd1.py
+++ examenedd1.py
@@ -1,68 +1,52 @@
-import numpy
+class Pila: 
+    top = None 
 
-distancias = numpy.array([[0,2050, 2015, 2190, 2725],
-[2050, 0, 119, 140, 675],
-[2015, 119, 0, 255, 790],
-[2190, 140, 255, 0, 535],
-[2725, 675, 790, 535, 0]])
-ciudades = numpy.array(['Arica', 'Santiago', 
-'Valparaiso', 'San Fernando', 'Temuco'])
+    def __init__(self): 
+        self.sig = None 
+        self.dato = None
 
-def ingresar_itinerario():
-	num_ciudades = input("Ingrese el numero de ciudades del itinerario: ")
-	num_ciudades = int(num_ciudades)
-	itinerario = []
-	print("Ingrese las ciudades del itinerario: ")
-	for c in range(0, num_ciudades):
-		itinerario.append(input())
-	return itinerario
-	
+    def push(self, dato): 
+        nuevo = Pila() 
+        nuevo.dato = dato 
+        nuevo.sig = Pila.top 
+        Pila.top = nuevo 
 
-def kms(itinerario):
-	# Calculo de las distancias del itinerario
-	d = 0
-	num_ciudades = len(itinerario)
-	for t in range(0, num_ciudades -1):
-		c1 = -1
-		c2 = -1
-		# Buscamos el índice que le corresponde a 
-		# la  primera ciudad (origen) 
-		for c in range(0,len(ciudades)):
-<<<<<<< HEAD
-		    if ciudades[c] == itinerario[t]:
-		        c1 = c
-                print ('es igual c1')
-		    if ciudades[c] == itinerario[t+1]:
-		        c2 = c
-				print('es igual c2')
-=======
-			if ciudades[c] == itinerario[t]:
-				c1 = c
-				break
-		# Buscamos el índice que le corresponde a 
-		# la segunda ciudad (destino)
-		for c in range(0,len(ciudades)):
-			if ciudades[c] == itinerario[t+1]:
-				c2 = c
-				break
->>>>>>> 02ac62df9636a408e53f556e08b8132250c1f9ae
-		d = d + distancias[c1][c2]
-		print (ciudades[c1], '-', ciudades[c2], distancias[c1][c2])
-	print('La distancia total es: ', d)
+    def pop(self): 
+        if self.vacia(): 
+            print("No hay elementos en la pila")
+        else: 
+            Pila.top = Pila.top.sig 
+            return Pila.top.dato
 
-itinerario = ingresar_itinerario()
-kms(itinerario)
+    def vacia(self): 
+        if Pila.top == None: 
+            return True 
+        else: 
+            return False 
 
-# Ejecucion:
-# pyton examenedd1.py
-# Ingrese el numero de ciudades del itinerario: 4
-# Ingrese las ciudades del itinerario: 
-# Temuco
-# Santiago
-# San Fernando
-# Arica
-# Temuco - Santiago 675
-# Santiago - San Fernando 140
-# San Fernando - Arica 2190
-# La distancia total es:  3005
+    def imprimir(self): 
+        tmp = Pila.top 
+        while tmp != None: 
+            if tmp.sig != None:
+                print (tmp.dato)
+            else:
+                print (tmp.dato)
+            tmp = tmp.sig
 
+    def longitud(self):
+        contador = 0
+        tmp = Pila.top 
+        while tmp != None: 
+            tmp = tmp.sig
+            contador = contador + 1
+        return contador
+
+p = Pila()
+p.push(4)
+p.push(5)
+p.push(45)
+p.imprimir()
+print('El tamaño de la pila es: ', p.longitud())
+p.pop()
+p.imprimir()
+print('El tamaño de la pila es: ', p.longitud())
---
+++ examenedd2.py
@@ -0,0 +1,59 @@
+import numpy
+
+distancias = numpy.array([[0,2050, 2015, 2190, 2725],
+[2050, 0, 119, 140, 675],
+[2015, 119, 0, 255, 790],
+[2190, 140, 255, 0, 535],
+[2725, 675, 790, 535, 0]])
+
+ciudades = numpy.array(['Arica', 'Santiago',
+'Valparaiso', 'San Fernando', 'Temuco'])
+
+def ingresar_itinerario():
+	num_ciudades = input("Ingrese el numero de ciudades del itinerario: ")
+	num_ciudades = int(num_ciudades)
+	itinerario = []
+	print("Ingrese las ciudades del itinerario: ")
+	for c in range(0, num_ciudades):
+		itinerario.append(input())
+	return itinerario
+
+def kms(itinerario):
+	# Calculo de las distancias del itinerario
+	d = 0
+	num_ciudades = len(itinerario)
+	for t in range(0, num_ciudades -1):
+		c1 = -1
+		c2 = -1
+		# Buscamos el índice que le corresponde a
+		# la primera ciudad (origen)
+		for c in range(0,len(ciudades)):
+			if ciudades[c] == itinerario[t]:
+				c1 = c
+				break
+		# Buscamos el índice que le corresponde a
+		# la segunda ciudad (destino)
+		for c in range(0,len(ciudades)):
+			if ciudades[c] == itinerario[t+1]:
+				c2 = c
+				break
+		d = d + distancias[c1][c2]
+		print (ciudades[c1], '-', ciudades[c2], distancias[c1][c2])
+	print('La distancia total es: ', d)
+
+itinerario = ingresar_itinerario()
+kms(itinerario)
+
+# Ejemplo de Ejecucion:
+#
+# pyton examenedd1.py
+# Ingrese el numero de ciudades del itinerario: 4
+# Ingrese las ciudades del itinerario:
+# Temuco
+# Santiago
+# San Fernando
+# Arica
+# Temuco - Santiago 675
+# Santiago - San Fernando 140
+# San Fernando - Arica 2190
+# La distancia total es: 3005
